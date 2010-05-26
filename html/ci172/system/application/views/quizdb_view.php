c0a3a6d8597cea0d9f04b3b9f0aa21477d6c3b25
timotheus <tim@cogmotive.com>
2010-05-26 14:52:27 -0700

    Added quizdb controller, and quizdb_view. Also added table and data to db, check NOTES for info.  The quizdb controller graps all questions of pack 1, randomly chooses one and displays.

---
+++ html/ci172/system/application/views/quizdb_view.php
@@ -0,0 +1,8 @@
+
+<h2>Quiz Time!</h2>
+<p>Write the the following word in <em>hiragana</em>.  Refresh for a new symbol.</p>
+
+<div class="quizprompt"><?php echo $question; ?></div>
+
+<p><a href="" id="slick-slidetoggle">Reveal the answer</a>
+<div id="slickbox"><span style="font-size: 2em;"><img src="<?php echo $answer; ?>"></span></div>
