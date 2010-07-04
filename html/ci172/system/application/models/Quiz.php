e59b1f7d7315c3ff5082137792cb9a22d39bcb02
Brian Tanaka <brian.tanaka@gmail.com>
2010-07-04 13:33:47 -0700

    created Quiz model, and moved db code from quizdb controller to it.  also: removed quizdb controller.

---
+++ html/ci172/system/application/models/Quiz.php
@@ -0,0 +1,21 @@
+<?php
+
+// TODO: the following code was removed from the quizdb controller. it needs to be refactored for model.
+//class Quizdb extends Controller {
+//
+//    function index()
+//    {
+//        $chosenpack=1;  //hardcoding to select from first pack, this can be fed later
+//        $this->load->database('dev');
+//        $query = $this->db->query('SELECT question, answer FROM quizdata WHERE pack=' .$this->db->escape($chosenpack));
+//        $randomrow = rand(0,$query->num_rows());  //getting random number between 0 and the number of rows returned
+//        $data = $query->row($randomrow);  //data of the random row
+//        $this->load->helper('html');
+//        $this->load->view('header_view');
+//        $this->load->view('menu_view');       
+//        $this->load->view('quizdb_view', $data);
+//        $this->load->view('footer_view');
+//    }
+//}
+?>
+
