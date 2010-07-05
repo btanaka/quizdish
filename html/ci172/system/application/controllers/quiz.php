6764235aaa7424920c31b6427c2349adc7772cbe
Brian Tanaka <brian.tanaka@gmail.com>
2010-07-05 13:21:09 -0700

    added kana type to path, added first few hiragana pngs.

--- html/ci172/system/application/controllers/quiz.php
+++ html/ci172/system/application/controllers/quiz.php
@@ -2,6 +2,8 @@
 class Quiz extends Controller {
 
 	var $imagepath = "/images/kana/";
+	var $imageext = ".png";
+	var $kanatype = "hiragana/";
 	
 	function Quiz() {
     	parent::Controller();
@@ -19,13 +21,13 @@ class Quiz extends Controller {
         $syllables = $this->quizdish->get_random_syllables();
 //        echo "$syllables"; //debug
 		$image_bases = explode(' ', $syllables);
-		// TODO: the current array supports three and only three syllables. 
-		// refactor to make it more flexible.
+		// TODO: the current array supports three and only three syllables. refactor to make it more flexible.
+		// TODO: only handles hiragana for now. refactor to handle katakana too optionally.
 		$data = array(
                'syllables' => $syllables,
-				'image01' => $this->imagepath . $image_bases[0] . ".jpg",
-				'image02' => $this->imagepath . $image_bases[1] . ".jpg",
-				'image03' => $this->imagepath . $image_bases[2] . ".jpg"
+				'image01' => $this->imagepath . $this->kanatype . $image_bases[0] . $this->imageext,
+				'image02' => $this->imagepath . $this->kanatype . $image_bases[1] . $this->imageext,
+				'image03' => $this->imagepath . $this->kanatype . $image_bases[2] . $this->imageext
 		);
         $this->load->view('quiz_view', $data); // TODO: current quiz_view is hard-wired. fix for dynamic.
         $this->load->view('footer_view');
