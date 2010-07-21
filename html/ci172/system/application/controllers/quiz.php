5a1472b0b04312ef42f3f6a6b8045773718d51f0
Brian Tanaka <brian.tanaka@gmail.com>
2010-07-20 22:45:31 -0700

    minor, fixing wacky eclipse indenting. ;-)

--- html/ci172/system/application/controllers/quiz.php
+++ html/ci172/system/application/controllers/quiz.php
@@ -1,34 +1,34 @@
 <?php
 class Quiz extends Controller {
 
-	var $imagepath = "/images/kana/";
-	var $imageext = ".png";
-	var $kanatype = "hiragana/";
+    var $imagepath = "/images/kana/";
+    var $imageext = ".png";
+    var $kanatype = "hiragana/";
 	
-	function Quiz() {
-    	parent::Controller();
-	}
+    function Quiz() {
+        parent::Controller();
+    }
 	
     function index()
     {
-		$this->load->helper('html');
-    	$this->load->library('quizdish');
+        $this->load->helper('html');
+        $this->load->library('quizdish');
 
-    	$this->load->view('header_view');
+        $this->load->view('header_view');
         $this->load->view('menu_view');
         
-		log_message('debug', "in quiz controller...");
+        log_message('debug', "in quiz controller...");
         $syllables = $this->quizdish->get_random_syllables();
-//        echo "$syllables"; //debug
-		$image_bases = explode(' ', $syllables);
-		// TODO: the current array supports three and only three syllables. refactor to make it more flexible.
-		// TODO: only handles hiragana for now. refactor to handle katakana too optionally.
-		$data = array(
-               'syllables' => $syllables,
-				'image01' => $this->imagepath . $this->kanatype . $image_bases[0] . $this->imageext,
-				'image02' => $this->imagepath . $this->kanatype . $image_bases[1] . $this->imageext,
-				'image03' => $this->imagepath . $this->kanatype . $image_bases[2] . $this->imageext
-		);
+        //echo "$syllables"; //debug
+	$image_bases = explode(' ', $syllables);
+	// TODO: the current array supports three and only three syllables. refactor to make it more flexible.
+	// TODO: only handles hiragana for now. refactor to handle katakana too optionally.
+	$data = array(
+            'syllables' => $syllables,
+            'image01' => $this->imagepath . $this->kanatype . $image_bases[0] . $this->imageext,
+            'image02' => $this->imagepath . $this->kanatype . $image_bases[1] . $this->imageext,
+            'image03' => $this->imagepath . $this->kanatype . $image_bases[2] . $this->imageext
+        );
         $this->load->view('quiz_view', $data); // TODO: current quiz_view is hard-wired. fix for dynamic.
         $this->load->view('footer_view');
     }
